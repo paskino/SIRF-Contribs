d1b021315bac8c155f0e10bc5aba6182a384fa5d
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2026-08-11 10:30:57 +0000

    update with SB devcontainer branch

--- src/notebooks/AIRBI-MRI-recon/mr_recon.sh
+++ src/notebooks/AIRBI-MRI-recon/mr_recon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-. /opt/SIRF-SuperBuild/INSTALL/bin/env_sirf.sh
-gadgetron &
+# Not necessary with SIRF-SuperBuild on devcontainer branch. 
+# . /opt/SIRF-SuperBuild/INSTALL/bin/env_sirf.sh
+# gadgetron &
 # python /workdir/reco_scripts/mr_direct_recon.py /input /output
 python stgeorges_pipeline.py
