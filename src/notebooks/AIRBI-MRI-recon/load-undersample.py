302f7e39bdf571c55e79634a225a3311beb4ba34
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2026-09-09 07:47:35 +0000

    updated and tested Wavelet functions

--- src/notebooks/AIRBI-MRI-recon/load-undersample.py
+++ src/notebooks/AIRBI-MRI-recon/load-undersample.py
@@ -79,7 +79,12 @@ am = {}
 ad_fs = AcquisitionData(output_files[0])
 ad_fs = preprocess_acquisition_data(ad_fs)
 am['fs'] = {'am': None, 'csm': None}
+#%%
+sum_fs = np.sum(np.abs(ad_fs.asarray().ravel()))
+# ad_fs /= sum_fs
 am['fs']['data'] = ad_fs
+
+print(f"Sum of Fully Sampled data: {sum_fs}")
 # %%
 from sirf.Gadgetron import FullySampledReconstructor
 which = "fs"
@@ -97,6 +102,12 @@ show2D(np.squeeze(np.abs(x_recon.asarray()[11,110:410, 80:380])).T)
 # AI protocol
 ad_ai = AcquisitionData(output_files[1])
 ad_ai = preprocess_acquisition_data(ad_ai)
+
+sum_ai = np.sum(np.abs(ad_ai.asarray().ravel()))
+# ad_ai /= sum_ai
+print(f"Sum of AI data: {sum_ai}")
+am['ai'] = {'am': None, 'csm': None, 'data':  ad_ai}
+
 # %%
 
 def get_AcquisitionModel_CSM(acq_data, smoothness=100, fully_sampled=True):
@@ -145,13 +156,15 @@ def get_AcquisitionModel_CSM(acq_data, smoothness=100, fully_sampled=True):
 
 #%%
 # Get the Coil Sensitivity Maps (CSM) from the fully sampled data
-E, csm_fs = get_AcquisitionModel_CSM(ad_fs, fully_sampled=True)
-am['fs'] = {'am': E, 'csm': csm_fs}
+E, csm_fs = get_AcquisitionModel_CSM(am['fs']['data'], fully_sampled=True)
+am['fs']['am'] = E
+am['fs']['csm'] = csm_fs
 #%%
-E, csm = get_AcquisitionModel_CSM(ad_ai, fully_sampled=range(-13,13))
-am['ai'] = {'am': E, 'csm': csm}
+E, csm = get_AcquisitionModel_CSM(am['ai']['data'], fully_sampled=range(-13,13))
+am['ai']['am'] = E
+am['ai']['csm'] = csm
 # am['ai']['norm'] = am['ai']['am'].norm()
-am['ai']['data'] = ad_ai
+
 # logger.info(f"Norm of E (AI): {am['ai']['norm']}")
 # %%
 # Undersample fully sample data with Gaussian variable density 
@@ -179,17 +192,30 @@ importlib.reload(stgeorges_utils)
 from stgeorges_utils import plot_kspace_lines_memory
 acq_data_us = create_gaussian_variable_density_kspace_data(am['fs']['data'],
                                                            acceleration_factor=4)
-
+am['us'] = {'data': acq_data_us}
 #%%
 plot_kspace_lines_memory([am['fs']['data'], am['ai']['data'], acq_data_us], None)
                         #  np.max(ky_index))
 
 # %%
-E, csm = get_AcquisitionModel_CSM(acq_data_us, fully_sampled=range(-13,13))
-am['us'] = {'am': E, 'csm': csm}
-am['us']['data'] = acq_data_us
-#%%
+# sum_us = np.sum(np.abs(acq_data_us.asarray().ravel()))
+# print(f"Sum of Undersampled data: {sum_us}")
+# acq_data_us /= sum_us
 
+E, csm = get_AcquisitionModel_CSM(acq_data_us, fully_sampled=range(-13,13))
+am['us']['am'] = E
+am['us']['csm'] = csm
+
+#%%
+# Data should be normalised now
+sum_ai = np.sqrt(np.sum(np.abs(am['ai']['data'].asarray().ravel())))
+sum_us = np.sqrt(np.sum(np.abs(am['us']['data'].asarray().ravel())))
+sum_fs = np.sqrt(np.sum(np.abs(am['fs']['data'].asarray().ravel())))
+print(f"Sum of AI data: {sum_ai}")
+print(f"Sum of Undersampled data: {sum_us}")
+print(f"Sum of Fully Sampled data: {sum_fs}")
+
+#%%
 from cil.optimisation.utilities.callbacks import Callback, ProgressCallback
 class LogAll(Callback):
     def __init__(self, interval=1, rmse_reference=None, rmse_border=20, plot=False):
@@ -233,45 +259,50 @@ class LogAll(Callback):
 # leading to a complete different scale of the reconstructed image and 
 # reconstruction parameter
 
-am['fs']['alpha'] = 3e-6
+am['fs']['alpha'] = 8.5e-06
 am['ai']['alpha'] = 3e-1
-am['us']['alpha'] = 3e-6
+am['us']['alpha'] = 8.5e-6
 
 #%%
 # Define our objective/loss function as least squares between Ex and y
-num_iterations = 10
+num_iterations = 60
+start_new = False
     
 for k,v in am.items():
-    if k in ['fs', 'ai']:
+    if k in ['fs']:
         logger.info(f"Skipping {k}")
         continue
     logger.info(f"Processing {k}")
     E = v['am']
     acq_data = v['data']
     x_init = E.inverse(acq_data)
-    x_init *= 0
+    # x_init *= 0
     f = LeastSquares(E, acq_data, c=1)
 
     alpha = v['alpha']
+    # alpha = 1
     TV = FGP_TV(alpha=alpha, nonnegativity=False, device='cpu', max_iteration=500)
     G = TV
 
+    logger.info(f"LS {f(x_init)}")
+    logger.info(f"TV {G(x_init)}")
     # add logger callback to FISTA
 
 
     # Set up FISTA
-    fista = FISTA(initial=x_init, f=f, g=G)
-    fista.update_objective_interval = 1
-    v['algo'] = fista
-
+    if start_new:
+        fista = FISTA(initial=x_init, f=f, g=G)
+        fista.update_objective_interval = 1
+        v['algo'] = fista
+    else:
+        fista = v['algo']
     # Run FISTA for least squares
     v['callback'] = LogAll(rmse_reference=x_recon, plot=True)
 
     fista.run(num_iterations, callbacks=[ProgressCallback(), v['callback']])
     
 
-# %%
-
+#
 #%%
 # Load Original Siemens AI reconstruction
 # Data loaded from DICOM files, exported by Slicer3D as NIfTI file
@@ -286,6 +317,7 @@ print (siemens_ai_recon.asarray().shape)
 #%%
 # Try to put the Siemens AI reconstruction into a lower resolution grid to match the undersampled data and fully sampled data
 binned = downscale_local_mean(siemens_ai_recon.asarray(), (2, 2, 1))  # keep Z, bin 2x2 in XY
+# find the image to approximately the same spot as the reconstructed images from the data with SIRF
 siemens_rebinned_recon = np.abs(np.fliplr(np.flipud(binned[110:410, 130:430,  6]))).T
 #%%
 show2D([siemens_rebinned_recon], title=['Siemens AI recon'])
@@ -297,7 +329,93 @@ show2D([np.squeeze(np.abs(x_recon.asarray()[11,110:410, 80:380])).T,
        title=['Fully Sampled', 'Siemens AI recon', f'AI data LS+{am["ai"]["alpha"]}TV',
               f'Undersampled LS+{am['us']['alpha']}TV'])
 
+#%%
+import matplotlib.pyplot as plt
+plt.plot(am['us']['algo'].loss)
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Loss Curve for Undersampled LS+TV Reconstruction')
+plt.show()
 # %%
 show2D([np.squeeze(np.abs(el.asarray()[11,110:410, 80:380])).T for el in [am['ai']['am'].inverse(am['ai']['data']), am['ai']['algo'].solution, am['us']['am'].inverse(am['us']['data']), am['us']['algo'].solution]], 
        title=['AI data inverse', 'AI data LS+TV', 'Undersampled inverse', 'Undersampled LS+TV'])
+# %%
+# histogram of acquisition data
+
+am['ai']['data_histo'] = np.histogram(np.abs(am['ai']['data'].as_array().ravel()))
+am['us']['data_histo'] = np.histogram(np.abs(am['us']['data'].as_array().ravel()))
+am['fs']['data_histo'] = np.histogram(np.abs(am['fs']['data'].as_array().ravel()))
+
+# %%
+sum_ai = np.sum(np.abs(am['ai']['data'].asarray().ravel()))
+sum_us = np.sum(np.abs(am['us']['data'].asarray().ravel()))
+sum_fs = np.sum(np.abs(am['fs']['data'].asarray().ravel()))
+print(f"Sum of AI data: {sum_ai}")
+print(f"Sum of Undersampled data: {sum_us}")
+print(f"Sum of Fully Sampled data: {sum_fs}")
+# %%
+# Clear temp dir
+# delete proc_dir
+if os.path.exists(proc_dir):
+    logger.info(f"Deleting temporary processing directory {proc_dir}...")
+    try:
+        for fname in os.listdir(proc_dir):
+            os.remove(os.path.join(proc_dir, fname))
+        os.rmdir(proc_dir)
+        logger.info("Temporary processing directory deleted successfully.")
+    except Exception as e:
+        logger.error(f"Error deleting temporary processing directory: {e}")
+# %%
+
+# TEST THE Wavelet stuff
+import importlib
+import SIRFWavelets
+importlib.reload(SIRFWavelets)
+from SIRFWavelets import FunctionOfAbs
+from cil.optimisation.operators import IdentityOperator
+from cil.optimisation.functions import L1Sparsity, L1Norm
+
+#%%
+data = am['us']['algo'].solution
+op = IdentityOperator(data)
+
+out1 = op.direct(data)
+out2 = op.direct(out1)
+
+show2D([np.squeeze(np.abs(el.asarray()[11,110:410, 80:380])).T for el in [data, out1, out2]])
+#%%
+l1s = L1Sparsity(op)
+l1n = L1Norm()
+tau = 5e-5
+
+print (l1s(data), l1n(data))
+print (l1s.proximal(data, tau, out=out1), l1n.proximal(data, tau, out=out2))
+show2D([np.squeeze(np.abs(el.asarray()[11,110:410, 80:380])).T for el in [out1, out2]])
+# %%
+importlib.reload(SIRFWavelets)
+from SIRFWavelets import FunctionOfAbs
+fabs1 = FunctionOfAbs(l1s)
+fabs2 = FunctionOfAbs(l1n)
+
+print(fabs1(data), fabs2(data))
+print(fabs1.proximal(data, tau, out=out1), fabs2.proximal(data, tau, out=out2))
+show2D([np.squeeze(np.abs(el.asarray()[11,110:410, 80:380])).T for el in [out1, out2]])
+# %%
+importlib.reload(SIRFWavelets)
+from SIRFWavelets import SIRFWaveletOperator
+
+wop = SIRFWaveletOperator(domain_geometry=am['us']['algo'].solution)
+
+out3 = wop.direct(am['us']['algo'].solution)
+out4 = wop.adjoint(out1)
+show2D([np.squeeze(np.abs(el.as_array()[11,110:410, 80:380])).T for el in [out3, out4]])
+
+# %%
+l1s = L1Sparsity(wop)
+
+tau = 5e-5
+out5 = l1s.proximal(am['us']['algo'].solution, tau)
+
+show2D([np.squeeze(np.abs(el.as_array()[11,110:410, 80:380])).T for el in [am['us']['algo'].solution, out5]])
+
 # %%
